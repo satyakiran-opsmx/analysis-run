350c477bebf50bcabd238a0c3d774e2023a8df14
Satya Kiran <satya.rayasam@opsmx.io>
2022-10-31 18:51:51 +0530

    check iframe page load

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -10,14 +10,7 @@ export const Extension = (props: {
   console.log("props: ",props);
   return (
     <div className="shopping-list">
-      <h1>Shopping List</h1>
-      <ul>
-        <li>Instagram</li>
-        <li>WhatsApp</li>
-        <li>Oculus</li>
-        <li>Facebook</li>
-      </ul>
-      <iframe src="http://localhost:4200/plugin-isd/verification/final-tests/1498/fromPlugin/200"></iframe>
+      <iframe src="http://localhost:4200/plugin-isd/verification/final-tests/1498/fromPlugin/200" style={{width: "100%", height: "100vh"}}></iframe>
     </div>
   );
 };
