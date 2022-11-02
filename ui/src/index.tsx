43d6899ebd209cb12a67da113646db94cc46fa80
Nagendra <nagendra.prasad@opsmx.io>
2022-11-02 11:15:45 +0530

    iframe post communicate

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 interface ApplicationResourceTree { }
 interface State { status: any }
