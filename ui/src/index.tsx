19d0e53ae078610002cd66d20aecea3b91087fa2
Nagendra <nagendra.prasad@opsmx.io>
2022-11-02 11:58:44 +0530

    iframe post communicate

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -8,9 +8,6 @@ export const Extension = (props: {
   tree: ApplicationResourceTree;
   resource: State;
 }) => {
-  useEffect(()=>{
-    window.top.postMessage('I am Iframe', '*')
-  },[])
  
   return (
     <div className="shopping-list">
@@ -21,7 +18,7 @@ export const Extension = (props: {
         <li>Oculus</li>
         <li>Facebook</li>
       </ul>
-      <iframe src="http://localhost:4200/argocontext/final-tests/121/argorollout.argo-dev.opsmx.net/deploymentverification/final-tests/1498/206?argoRollout=true" className="myIframe"></iframe>
+      <iframe src="http://localhost:4200/plugin-isd/verification/final-tests/1498/fromPlugin/200" ></iframe>
     </div>
   );
 };
