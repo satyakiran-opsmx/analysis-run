b01db4974b9faaf1e9adce2fff7ea33e10ff19e4
Nagendra <nagendra.prasad@opsmx.io>
2022-11-02 11:12:24 +0530

    iframe post communicate

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -8,47 +8,22 @@ export const Extension = (props: {
   tree: ApplicationResourceTree;
   resource: State;
 }) => {
-  const [noReport, setReportStatus] = useState(false);
-  useEffect(() => {
-    let latestCanaryId = null;
-    if (props.resource.status.metricResults && props.resource.status.metricResults.length) {
-      latestCanaryId = props.resource.status.metricResults[props.resource.status.metricResults.length - 1];
-    }
-    if (latestCanaryId) {
-      setReportStatus(true);
-      let fetchStatusUrl = 'https://isd-dev.argo-dev.opsmx.net/gate/autopilot/api/v2/applications/getApplicationHealth?canaryId=' + latestCanaryId;
-      fetch(fetchStatusUrl)
-        .then(response => {
-          return response.json();
-        })
-        .then((data: any) => {
-          if (data) {
-            console.log('getApplicationHealth');
-            console.log(JSON.parse(data));
-          }
-        }).catch(err => {
-          console.error('res.data', err)
-        });
-    }
-    else {
-      setReportStatus(false)
-    }
-  }, [])
-
-  console.log("props: ", props);
-  return <>
-    {!noReport ? <h1>No Report Available</h1> :
-      <div className="shopping-list">
-        <h1>Shopping List</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-          <li>Facebook</li>
-        </ul>
-      </div>
-    }
-  </>
+  useEffect(()=>{
+    window.top.postMessage('I am Iframe', '*')
+  },[])
+ 
+  return (
+    <div className="shopping-list">
+      <h1>Shopping List</h1>
+      <ul>
+        <li>Instagram</li>
+        <li>WhatsApp</li>
+        <li>Oculus</li>
+        <li>Facebook</li>
+      </ul>
+      <iframe src="http://localhost:4200/application" className="myIframe"></iframe>
+    </div>
+  );
 };
 
 export const component = Extension;
