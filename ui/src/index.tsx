098b7ee9be4aba677eca470e6a1761e463b21c70
Satya Kiran <satya.rayasam@opsmx.io>
2022-11-04 14:45:36 +0530

    Updated iFrame url

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -10,7 +10,7 @@ export const Extension = (props: {
   console.log("props: ",props);
   return (
     <div className="shopping-list">
-      <iframe src="https://isd-dev.argo-dev.opsmx.net/ui/plugin-isd/verification/final-tests/1498/fromPlugin/200" style={{width: "100%", height: "100vh"}}></iframe>
+      <iframe src="https://isd.argo.ninja-test.opsmx.org/ui/application" style={{width: "100%", height: "100vh"}}></iframe>
     </div>
   );
 };
